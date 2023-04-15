69b10ec2bd8d2ebfbd2ad6c78c580d97b24364e2
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-04-15 22:12:08 +0300

    Initial commit

---
+++ app/src/main/java/com/example/factorialtest/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.factorialtest
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import com.example.factorialtest.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        observeViewModel()
+        binding.buttonCalculate.setOnClickListener {
+            viewModel.calculate(binding.editTextNumber.text.toString())
+        }
+    }
+
+    private fun observeViewModel() {
+        viewModel.progress.observe(this) {
+            if (it) {
+                binding.progressBarLoading.visibility = View.VISIBLE
+                binding.buttonCalculate.isEnabled = false
+            } else {
+                binding.progressBarLoading.visibility = View.GONE
+                binding.buttonCalculate.isEnabled = true
+            }
+        }
+        viewModel.error.observe(this) {
+            if (it) {
+                Toast.makeText(
+                    this,
+                    "You didn't entered value",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
+        viewModel.factorial.observe(this) {
+            binding.textViewFactorial.text = it
+        }
+    }
+}
